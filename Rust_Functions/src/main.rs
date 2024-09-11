e28d4e0d40b92300bec9699befd3fb7d9b08378a
P3N5IVE_CH3PE <P3N5IVE_CH3PE@protonmail.com>
2024-09-10 18:06:20 -0700

    Added long form of comment in Rust

--- Rust_Functions/src/main.rs
+++ Rust_Functions/src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
    
+
+
 				let x = plus_one(5);
 
 				println!("The value of x is: {x}");
 }
 
 fn plus_one(x: i32)-> i32{
+
+/*If you add a semicolon, it becomes a statements, returns no value;If you remove it, returns value */
 				x + 1;
 
 }
